a9469c6af87f24fe7abf8225a90c79fcccea0e5a
Wiliam Souza <wiliamsouza83@gmail.com>
2018-05-27 15:01:29 -0300

    Change restart strategies to permanent avoiding acceptor crash

--- lib/kv_server/application.ex
+++ lib/kv_server/application.ex
@@ -12,7 +12,7 @@ defmodule KVServer.Application do
 
     children = [
       {Task.Supervisor, name: KVServer.TaskSupervisor},
-      {Task, fn -> KVServer.accept(port) end}
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
